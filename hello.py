dba9027fdb98d87ccee1602b08837a825e3c9383
Abhishek p <Abhishek p>
2016-09-13 15:21:16 +0530

    navbar

--- hello.py
+++ hello.py
@@ -15,12 +15,14 @@ def login_check():
 	username = request.form["username"];
 	password = None
 	password = request.form["password"];
-	value = "You are " + username 
+	value = "<body>You are " + username 
 	if not password =="benovelance": 
 		value = value + ". You don't have a password. So, bring a stash of golden apples to start  with ReaSE."
 	else:
 		value = value + ". You have a pasword, nice.. but ReaSE isn't up yet. So wait a little longer..."
 	print "p--" + password
+	
+	value = value + "</body>"
 	return value
 if __name__ == "__main__":
     app.run(debug=True)
