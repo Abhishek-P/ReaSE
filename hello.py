1bbcb73400012b6b78a67cebd32a82c6b7656818
Abhishek p <Abhishek p>
2016-09-23 20:48:20 +0530

    Functional login

--- hello.py
+++ hello.py
@@ -16,17 +16,14 @@ def login_check():
 	username = request.form["username"];
 	password = None
 	password = request.form["password"];
-	value = "<body>You are " + username 
-	if not password =="benovelance": 
-		value = "false"
-	else:
-		return redirect("http://localhost:1729/dashboard",code = 200)
+	if password =="benovelance": 
+		 return "http://localhost:1729/"
 	
-	return value
+	return "false"
 	
 @app.route("/dashboard",methods = ['GET'])
 def dashboard():
-	return "WOW"
+	return "<html></html>"
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT',1729))
 	app.run(host = "127.0.0.1",port = port ,debug=True)
